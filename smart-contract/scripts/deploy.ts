9cdb9045facd7822d0367d8c29c6c7c51f4a437b
endlessorhub <anthonyhutchi99@gmail.com>
2023-05-17 23:26:18 +0530

    config added of local network and script added for deployment

--- smart-contract/scripts/deploy.ts
+++ smart-contract/scripts/deploy.ts
@@ -1,19 +1,13 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+  const OrchidzBuildNft = await ethers.getContractFactory("OrchidzBuildNft");
+  const orchidzBuildNft = await OrchidzBuildNft.deploy();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await orchidzBuildNft.deployed();
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("nft contract deployed on address: ",orchidzBuildNft.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -22,3 +16,6 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+
+// npx hardhat run scripts/deploy.ts --network ganache
