a20832b11c585107f61fcb427d8357380992b407
endlessorhub <anthonyhutchi99@gmail.com>
2023-05-21 12:45:00 +0530

    basic erc1155 contract added

---
+++ smart-contract/contracts/OrchidzBuildCreatorERC1155.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC1155, Ownable {
+  string public name;
+  string public symbol;
+
+  mapping(uint256=>bool) public allowedTokens;
+
+
+    constructor(string memory _name, string memory symbol) ERC1155("ipfs://_cid_/{id}.json") {
+        name = _name;
+        symbol = _symbol;
+    }
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+}
